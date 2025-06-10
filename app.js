684e26e3f8b95e86801335ec64536b674b702ea2
HALLakmina <lahirulakmina1999@gmail.com>
2025-06-10 17:18:27 +0530

    Create user routs, services module, validation utils, validator middleware hash password creator and install nodemon, dotenv, joi, and mongoose libraries

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 app.use(cors())
+
+main().catch(err=>console.log(err))
+
+async function main(params) {
+  await mongoose.connect(process.env.MONGO_URL)
+}
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
